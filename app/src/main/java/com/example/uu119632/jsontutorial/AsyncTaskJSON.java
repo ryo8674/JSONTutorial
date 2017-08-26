fd0bebe7bbf3356e61465b4fa5d99d9c93245ebd
ryo8674 <celtic_05_yma_17@yahoo.co.jp>
2017-08-26 21:43:47 +0900

    パラメータ修正

--- app/src/main/java/com/example/uu119632/jsontutorial/AsyncTaskJSON.java
+++ app/src/main/java/com/example/uu119632/jsontutorial/AsyncTaskJSON.java
@@ -17,21 +17,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by uu119632 on 2017/08/24.
+ * JSonを取得し、パースするクラス
+ *
+ * @author :ryo.yamada
+ * @since :1.0 :2017/08/25
  */
-
-public class AsyncTaskJSON extends AsyncTask<String, Void, List<String>> {
+class AsyncTaskJSON extends AsyncTask<String, Void, List<String>> {
 
     private MainActivity myActivity;
     private static final String REQUEST_METHOD = "GET";
     private static final String CHAR_CODE = "UTF-8";
-    private static final String BLANK = "";
     private static final String FAILED_SEARCH = "検索に失敗しました。";
     private static final int REQUEST_SUCCESS_CODE = 200;
 
-    private static final String GRAPH_KEY = "@graph";
-    private static final String ITEMS_KEY = "items";
-    private static final String TITLE_KEY = "title";
+    private static final String RESULTS_KEY = "results";
+    private static final String LARGE_AREA_KEY = "large_area";
+    private static final String SERVICE_AREA_KEY = "service_area";
+    private static final String NAME_KEY = "name";
 
 
     /**
@@ -80,19 +82,18 @@ public class AsyncTaskJSON extends AsyncTask<String, Void, List<String>> {
                 sb.append(line);
             }
 
-            // 取得した文字列からjsonobjectを作成
+            // 取得した文字列からJSonobjectを作成
             JSONObject jsonObject = new JSONObject(sb.toString());
 
             // キー配下の配列を取得
-            JSONObject resultsJsonObject = jsonObject.getJSONObject("results");
-            JSONArray largeAreaJsonArray = resultsJsonObject.getJSONArray("large_area");
+            JSONObject resultsJsonObject = jsonObject.getJSONObject(RESULTS_KEY);
+            JSONArray largeAreaJsonArray = resultsJsonObject.getJSONArray(LARGE_AREA_KEY);
 
-            StringBuilder builder = new StringBuilder();
             List<String> list = new ArrayList<>();
             // 配列の内容を順に取得し、titleキーを読み込み
             for (int i = 0; i < largeAreaJsonArray.length(); i++) {
-                JSONObject item = largeAreaJsonArray.getJSONObject(i);
-                list.add(item.getString("name"));
+                JSONObject item = largeAreaJsonArray.getJSONObject(i).getJSONObject(SERVICE_AREA_KEY);
+                list.add(item.getString(NAME_KEY));
             }
 
             return list;
--- app/src/main/java/com/example/uu119632/jsontutorial/MainActivity.java
+++ app/src/main/java/com/example/uu119632/jsontutorial/MainActivity.java
@@ -3,25 +3,39 @@ package com.example.uu119632.jsontutorial;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.TextView;
 
+/**
+ * メイン画面
+ *
+ * @author :ryo.yamada
+ * @since :1.0 :2017/08/25
+ */
 public class MainActivity extends Activity {
 
-    private TextView resultText;
+    private static final String HTTP_PROTOCOL = "http";
+    private static final String AUTHORITY = "webservice.recruit.co.jp";
+    private static final String PATH = "hotpepper/large_area/v1";
+    private static final String USER_PARAMETER = "key";
+    private static final String USER_KEY = "8928fba69a934d6e";
+    private static final String FORMAT_PARAMETER = "format";
+    private static final String FORMAT_KEY = "json";
 
+    /**
+     * onCreate
+     * @param savedInstanceState saveInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        http://webservice.recruit.co.jp/hotpepper/large_area/v1?key=8928fba69a934d6e&format=json
+
         //URIを生成
         Uri.Builder uriBuilder = new Uri.Builder();
-        uriBuilder.scheme("http");
-        uriBuilder.authority("webservice.recruit.co.jp");
-        uriBuilder.path("hotpepper/large_area/v1");
-        uriBuilder.appendQueryParameter("key", "8928fba69a934d6e");
-        uriBuilder.appendQueryParameter("format", "json");
-//        uriBuilder.appendQueryParameter("name", prefName);
+        uriBuilder.scheme(HTTP_PROTOCOL);
+        uriBuilder.authority(AUTHORITY);
+        uriBuilder.path(PATH);
+        uriBuilder.appendQueryParameter(USER_PARAMETER, USER_KEY);
+        uriBuilder.appendQueryParameter(FORMAT_PARAMETER, FORMAT_KEY);
 
         String utiStr = uriBuilder.toString();
 
